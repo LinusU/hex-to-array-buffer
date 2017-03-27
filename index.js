9cc8c40aa4be71035cf9e5c1d683fd84f239aae6
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2017-03-27 16:55:39 +1300

    Added export shim.
    Added export support for Node.js/Browser/UMD

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-module.exports = function hexToArrayBuffer (hex) {
-  if (typeof hex !== 'string') {
-    throw new TypeError('Expected input to be a string')
+(function () {
+  function hexToArrayBuffer (hex) {
+    if (typeof hex !== 'string') {
+      throw new TypeError('Expected input to be a string')
+    }
+
+    if ((hex.length % 2) !== 0) {
+      throw new RangeError('Expected string to be an even number of characters')
+    }
+
+    var view = new Uint8Array(hex.length / 2)
+
+    for (var i = 0; i < hex.length; i += 2) {
+      view[i / 2] = parseInt(hex.substring(i, i + 2), 16)
+    }
+
+    return view.buffer
   }
 
-  if ((hex.length % 2) !== 0) {
-    throw new RangeError('Expected string to be an even number of characters')
+   /**
+   * Exporting and stuff
+   */
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = hexToArrayBuffer;
+  } else {
+    if (typeof define === 'function' && define.amd) {
+      define([], function() {
+        return hexToArrayBuffer;
+      });
+    } else {
+      window.hexToArrayBuffer = hexToArrayBuffer;
+    }
   }
-
-  var view = new Uint8Array(hex.length / 2)
-
-  for (var i = 0; i < hex.length; i += 2) {
-    view[i / 2] = parseInt(hex.substring(i, i + 2), 16)
-  }
-
-  return view.buffer
-}
+})()
